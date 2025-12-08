e250f95664506344abb6614327561adad8059e78
☁  hyonna_portfolio [main] git config --global user.email yha0118@naver.com <yha0118@naver.com>
2025-12-08 23:01:59 +0900

    update: favicon 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,14 @@ import { Providers } from './Providers'
 
 export const metadata = {
   title: 'Moon Diary - 오늘의 감정, 달로 기록하다',
-  description: '오늘의 감정, 달로 기록하다'
+  description: '오늘의 감정, 달로 기록하다',
+  icons: {
+    icon: [
+      { url: '/icon.svg', type: 'image/svg+xml' },
+      { url: '/favicon.ico', type: 'image/x-icon' }
+    ],
+    apple: '/icon.svg',
+  }
 }
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
